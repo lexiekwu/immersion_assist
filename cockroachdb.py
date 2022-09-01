286960fda5f66de7f80c74e7a348068d4ed6e274
lexiekwu <lexiekwu@gmail.com>
2022-08-31 17:14:38 -0700

    basic flashcard functionality

---
+++ cockroachdb.py
@@ -0,0 +1,48 @@
+from os import environ
+import psycopg2
+import psycopg2.extras
+
+DB_URL = "".join(
+    [
+        "postgresql://",
+        environ.get("COCKROACHDB_USER", ""),
+        ":",
+        environ.get("COCKROACHDB_PW", ""),
+        "@free-tier4.aws-us-west-2.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3D",
+        environ.get("COCKROACHDB_CLUSTER"),
+    ]
+)
+
+# init client
+db_conn = psycopg2.connect(DB_URL)
+db_cursor = db_conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+
+def sql_query(sql):
+    try:
+        db_cursor.execute(sql)
+        db_conn.commit()
+        result = db_cursor.fetchall()
+        return result
+    except psycopg2.Error as e:
+        db_cursor.execute("ROLLBACK")
+        raise e
+
+
+def sql_update(sql):
+    try:
+        db_cursor.execute(sql)
+        db_conn.commit()
+    except psycopg2.Error as e:
+        db_cursor.execute("ROLLBACK")
+        raise e
+
+
+def sql_update_multi(sql_list):
+    try:
+        for sql in sql_list:
+            db_cursor.execute(sql)
+        db_conn.commit()
+    except psycopg2.Error as e:
+        db_cursor.execute("ROLLBACK")
+        raise e
--- static/script.js
+++ static/script.js
@@ -4,4 +4,13 @@ window.addEventListener('load', function () {
 
   console.log("Hello World!");
 
+  var en_word_button = document.getElementById("@english_word_button")
+  var en_word_input = document.getElementById("@english_word_input")
+  en_word_button.addEventListener('click', function () {
+
+    console.log("clicked button")
+    console.log(en_word_input.value)
+
+  })
+
 });
