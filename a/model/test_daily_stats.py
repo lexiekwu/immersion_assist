c413adc1572a8326522b02c771f6957aa964c913
lexiekwu <lexiekwu@gmail.com>
2022-12-19 21:27:04 -0800

    improve test coverage for daily stats

--- a/model/test_daily_stats.py
+++ a/model/test_daily_stats.py
@@ -24,10 +24,11 @@ class TestDailyStats:
         assert ds.get_accuracy() == 0
 
     def test_get_for_day(self):
+        tds = daily_stats.DailyStats.get_for_day(self.test_dt)
+        assert (tds.count_correct, tds.count_incorrect, tds.dt) == (0, 0, self.test_dt)
 
         daily_stats.DailyStats(self.test_dt, 5, 10).update(False)
         daily_stats.DailyStats(self.today_dt, 6, 8).update(True)
-
         assert daily_stats.DailyStats.get_for_day(self.test_dt).count_correct == 5
         assert (
             daily_stats.DailyStats.get_for_day(self.test_dt).count_incorrect == 10 + 1
@@ -52,6 +53,18 @@ class TestDailyStats:
 
         ds.update(False)
         assert (ds.count_correct, ds.count_incorrect) == (6, 12)
+
+        ds = daily_stats.DailyStats(self.today_dt, 2, 3)
+        assert (ds.count_correct, ds.count_incorrect) == (2, 3)
+
+        ds.update(True)
+        assert (ds.count_correct, ds.count_incorrect) == (3, 3)
+
+        ds.update(True)
+        assert (ds.count_correct, ds.count_incorrect) == (4, 3)
+
+        ds.update(False)
+        assert (ds.count_correct, ds.count_incorrect) == (4, 4)
 
     def teardown_method(self):
         self.test_user.delete_all_data()
