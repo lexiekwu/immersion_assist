d6f698fccd48ba2a5c91bb69375787c15eb99b67
lexiekwu <lexiekwu@gmail.com>
2024-02-20 14:45:23 -0800

    meant to include in last diff

--- a/controller/test_chatbot.py
+++ a/controller/test_chatbot.py
@@ -3,17 +3,6 @@ from a.third_party import api_wrap, session_storage
 import uuid
 
 
-class FakeMessage:
-    def __init__(self, content):
-        self.content = content
-
-
-class FakeResponse:
-    def __init__(self, usage, content):
-        self.usage = usage
-        self.choices = [FakeMessage(content)]
-
-
 class TestChatBot:
     def setup_method(self):
         session_storage.set("uid", uuid.uuid4())
